c2960c41689559047031bd0934ef664bda57a73e
Bernardo Moya <bernardo_mm@hotmail.com>
2021-03-18 17:49:38 -0600

    Added auth middleware

--- src/index.js
+++ src/index.js
@@ -7,21 +7,18 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') res.send('GET requests are disabled')
+//     else next()
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('App is in maintenance')
+// })
+
 app.use(express.json())
 app.use(userRouter, taskRouter)
 
 app.listen(port, () => {
     console.log('Server is running on port ' + port)
 })
-
-const jwt = require('jsonwebtoken')
-
-const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '1 seconds' })
-    console.log(token)
-
-    const data = jwt.verify(token, 'thisismynewcourse')
-    console.log(data)
-}
-
-myFunction()

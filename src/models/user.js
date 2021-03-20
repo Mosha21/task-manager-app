79fee27ae6a5a59941471dbcde8af2b4eefcf3f2
Bernardo Moya <bernardo_mm@hotmail.com>
2021-03-19 22:35:14 -0600

    Finish adding auth to endpoints, added owner to tasks and cascade deletion to tasks when user removed

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -49,15 +50,11 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
-userSchema.methods.generateAuthToken = async function () {
-    const user = this
-    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
-    
-    user.tokens = user.tokens.concat({ token })
-    await user.save()
-
-    return token
-}
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+})
 
 userSchema.methods.toJSON = function () {
     const user = this
@@ -67,6 +64,16 @@ userSchema.methods.toJSON = function () {
     delete userObject.tokens
 
     return userObject
+}
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+    
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
 }
 
 userSchema.statics.findByCredentials = async (email, password) => {
@@ -89,6 +96,13 @@ userSchema.pre('save', async function (next) {
         user.password = await bcrypt.hash(user.password, 8)
     }
 
+    next()
+})
+
+// Delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+    const user = this
+    await Task.deleteMany({ owner: user._id })
     next()
 })
 
--- src/routers/task.js
+++ src/routers/task.js
@@ -1,46 +1,13 @@
 const express = require('express')
 const router = new express.Router()
-
+const auth = require('../middleware/auth')
 const Task = require('../models/task')
 
-router.patch('/tasks/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['description', 'completed']
-    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
-
-    if (!isValidOperation) return res.status(400).send({ 'error': 'Invalid update!' })
-
-    try {
-        const task = await Task.findById(req.params.id)
-
-        updates.forEach(update => task[update] = req.body[update])
-
-        await task.save() //This DOES call the middleware
-
-        //const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-
-        if (!task) return res.status(404).send()
-
-        res.send(task)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
-
-router.delete('/tasks/:id', async (req, res) => {
-    try {
-        const task = await Task.findByIdAndDelete(req.params.id)
-
-        if(!task) return res.status(404).send()
-
-        res.send(task)
-    } catch (error) {
-        res.status(500).send(error)
-    }
-})
-
-router.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
+router.post('/tasks', auth, async (req, res) => {
+    const task = new Task({
+        ...req.body, //this copies the values in req.body
+        owner: req.user._id
+    })
 
     try {
         await task.save()
@@ -50,20 +17,55 @@ router.post('/tasks', async (req, res) => {
     }
 })
 
-router.get('/tasks', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+
+    if (!isValidOperation) return res.status(400).send({ 'error': 'Invalid update!' })
+
     try {
-        const tasks = await Task.find({})
-        res.send(tasks)
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
+
+        //const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!task) return res.status(404).send()
+
+        updates.forEach(update => task[update] = req.body[update])
+        await task.save() //This DOES call the middleware
+        res.send(task)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.delete('/tasks/:id', auth, async (req, res) => {
+    try {
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
+
+        if(!task) return res.status(404).send()
+
+        res.send(task)
     } catch (error) {
         res.status(500).send(error)
     }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
+    try {
+        //const tasks = await Task.find({ owner: req.user._id }) //Same result
+        await req.user.populate('tasks').execPopulate()
+        res.send(req.user.tasks)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
 
     try {
-        const task = await Task.findById({ _id })
+        const task = await Task.findOne({ _id, owner: req.user._id })
 
         if(!task) {
             return res.status(404).send()
